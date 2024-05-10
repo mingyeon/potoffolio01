905935d50aa094657220d3cf5a91dad40f255f45
mingyeon.github.io <jojo9608@naver.com>
2024-05-10 16:18:30 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+const mainVisualSlide = new Swiper('.main-visual-slide', {
+    loop: true,
+    navigation: {
+        nextEl: '.main-visual-slide .arrows .next',
+        prevEl: '.main-visual-slide .arrows .prev',
+    },
+})
