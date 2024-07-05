6d39bfa64e0c422e26562b890c87c135d5cf334c
mingyeon.github.io <jojo9608@naver.com>
2024-07-05 15:32:24 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,13 @@
 $(function () {
-    $(window).on('scroll', function (e) {
-        e.preventDefault();
-        let sct = $(window).scrollTop();
-        if (sct > 0) {
-            $('#header').addClass('on')
-        } else {
-            $('#header').removeClass('on')
-        }
-    });
-
-
     $('#header .lang>a').on('click', function (e) {
         e.preventDefault();
+        $(this).next().toggle();
+    });
 
-        $('#header .lnk').toggle();
+    $('#bgVideo').YTPlayer({
+        videoURL: "https://www.youtube.com/embed/krwiZ7q9xC8",
+        containment: '.main_movie',
+        showControls: false,
+        playOnlyIfVisible: true,
     })
-
-    $('.main_product_slide').slick({
-        slidesToShow: 4,
-        arrows: false,
-    });
-
-    $('#main_product .arrows .prev').on('click', function () {
-        $('.main_product_slide').slick('slickPrev')
-    });
-
-    $('#main_product .arrows .next').on('click', function () {
-        $('.main_product_slide').slick('slickNext')
-    });
-})
+});
