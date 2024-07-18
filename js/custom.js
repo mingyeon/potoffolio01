e71994ba7b5697cc8a515615fe6975c93d6071db
mingyeon.github.io <jojo9608@naver.com>
2024-07-18 12:19:23 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,51 @@
 $(function () {
-    $('#header .lang>a').on('click', function (e) {
+    $(window).on('scroll', function (e) {
         e.preventDefault();
-        $(this).next().toggle();
+        let sct = $(window).scrollTop();
+        if (sct > 0) {
+            $('#header').addClass('on')
+        } else {
+            $('#header').removeClass('on')
+        }
     });
 
-    $('#bgVideo').YTPlayer({
-        videoURL: "https://www.youtube.com/embed/krwiZ7q9xC8",
-        containment: '.main_movie',
-        showControls: false,
-        playOnlyIfVisible: true,
+    $('#header .lang>a').on('click', function (e) {
+        e.preventDefault();
+        $('#header .lnk').toggle();
     })
-});
+
+    $(window).on('resize', function () {
+        $('#header #gnb').removeClass('on');
+        $('#header ').removeAttr('style')
+    })
+
+    $('#header .mbtn').on('click', function () {
+        $(this).toggleClass('on');
+        $('#header #gnb').toggleClass('on');
+    })
+
+    $('.main_product_slide').slick({
+        slidesToShow: 4,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+
+                }
+            }
+        ]
+    });
+
+    $('#main_product .arrows .prev').on('click', function () {
+        $('.main_product_slide').slick('slickPrev')
+    });
+
+    $('#main_product .arrows .next').on('click', function () {
+        $('.main_product_slide').slick('slickNext')
+    });
+
+
+})
